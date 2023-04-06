14ee6d0bf798930457f065193f505a0e6f460a4d
KevinBett <kevinbett458@gmail.com>
2023-04-06 20:34:42 +0300

    listed my api url and api key constants

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+const currentApiBaseUrl = 'https://api.weatherapi.com/v1/current.json';
+const forecastApiBaseUrl = 'https://api.weatherapi.com/v1/forecast.json';
+const apiKey = '8120596cefc941678a355320230604';
